4919fa1bc0cc02c80cda085b38a5f610eef144a3
Chris Wolf <chriswolfdesign@gmail.com>
2022-10-20 18:02:14 -0400

    Implemented Stack.Push

--- stack/stack.go
+++ stack/stack.go
@@ -1,9 +1,31 @@
 package stack
 
 type Stack struct {
-	Size int
+	size int
 	head *stackNode
 	tail *stackNode
 }
 
-type stackNode struct{}
+func (s *Stack) Size() int {
+	return s.size
+}
+
+func (s *Stack) Push(element int) {
+	newNode := &stackNode{val: element}
+	if s.size == 0 {
+		s.head = newNode
+		s.tail = newNode
+	} else {
+		s.tail.next = newNode
+		newNode.prev = s.tail
+		s.tail = newNode
+	}
+
+	s.size++
+}
+
+type stackNode struct {
+	val  int
+	next *stackNode
+	prev *stackNode
+}
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -4,18 +4,53 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"go-stack/stack"
+	stackLib "go-stack/stack"
 )
 
 var _ = Describe("Stack", func() {
-	var stack stack.Stack
+	var stack stackLib.Stack
+
+	BeforeEach(func() {
+		stack = stackLib.Stack{}
+	})
 
 	Describe("Initialization tests", func() {
 		When("Stack is initialized", func() {
 			It("Has a size of 0", func() {
-				Expect(stack.Size).To(Equal(0))
+				Expect(stack.Size()).To(Equal(0))
 			})
 		})
 	})
 
+	Describe("Push tests", func() {
+		When("pushing to empty stack", func() {
+			It("has size 1", func() {
+				stack.Push(1)
+				Expect(stack.Size()).To(Equal(1))
+			})
+		})
+
+		When("pushing to stack with one element", func() {
+			BeforeEach(func() {
+				stack.Push(1)
+			})
+
+			It("has size 1", func() {
+				stack.Push(2)
+				Expect(stack.Size()).To(Equal(2))
+			})
+		})
+
+		When("pushing to stack with multiple elements", func() {
+			BeforeEach(func() {
+				stack.Push(1)
+				stack.Push(2)
+			})
+
+			It("has size 2", func() {
+				stack.Push(3)
+				Expect(stack.Size()).To(Equal(3))
+			})
+		})
+	})
 })
